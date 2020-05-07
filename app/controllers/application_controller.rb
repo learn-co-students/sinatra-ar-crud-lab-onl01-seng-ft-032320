97a2cfb4198f7f97d20ac45b8ba56ec77878dfa7
github username <github email address>
2020-05-06 22:27:15 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,47 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do
+    @articles = Article.all
+    erb :index
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+      erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    @article.update(title: params[:title], content: params[:content])
+    # @article.title = params[:title]
+    # @article.content = params[:content]
+    # @article.save
+    redirect "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    
+    @article = Article.find_by_id(params[:id])
+
+    @article.delete
+    
+    erb :index
+  end
+
+
 end
